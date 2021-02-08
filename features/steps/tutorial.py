f4bb4b16e496c17e030cbc0cdf72b5e1c0382970
Jed Cainglet <jedcainglet@gmail.com>
2021-02-07 22:21:25 -0500

    Finish adding Tutorial Fixes #1

--- features/steps/tutorial.py
+++ features/steps/tutorial.py
@@ -1,5 +1,8 @@
 from behave import *
 
+# Feature: showing off behave
+
+# Scenario: run a simple test
 @given('we have behave installed')
 def step_impl(context):
     pass
@@ -12,7 +15,7 @@ def step_impl(context):
 def step_impl(context):
     assert context.failed is False
 
-
+# Scenario: explaining behave
 @given('we put the system in a known state')
 def step_impl(context):
     pass
@@ -25,7 +28,7 @@ def step_impl(context):
 def step_impl(context):
     assert context.failed is False
 
-
+# Scenario Outline: Blenders
 @given('I put Red Tree Frog in a blender')
 def step_impl(context):
     pass
@@ -66,3 +69,51 @@ def step_impl(context):
 # @then('it should transform into toxic waste')
 # def step_impl(context):
 #     assert context.failed is False
+
+# Scenario: some scenario
+@given('a sample text loaded into the frobulator')
+def step_impl(context):
+    print(context.text)
+    pass
+
+@when('we activate the frobulator')
+def step_impl(context):
+    assert True is not False
+
+@then('we will find it similar to English')
+def step_impl(context):
+    assert context.failed is False
+
+# Scenario: another some scenario
+@given('a set of specific users')
+def step_impl(context):
+    for row in context.table:
+        print(row['name'], "--", row['department'])
+
+@when('we count the number of people in each department')
+def step_impl(context):
+    assert True is not False
+
+@then('we will find two people in "Silly Walks"')
+def step_impl(context):
+    assert context.failed is False
+
+@then('we will find one person in "Beer Cans"')
+def step_impl(context):
+    assert context.failed is False
+
+# Scenario series: look up book
+@given('I search for a valid book')
+def step_impl(context):
+    context.response = "success"
+    pass
+
+@given('I search for an invalid book')
+def step_impl(context):
+    context.response = "failure"
+    pass
+
+@then('the result page will include "{text}"')
+def step_impl(context, text):
+    if text not in context.response:
+        raise Exception(f"expected '{context.response}', found '{text}' instead")
--- features/tutorial.feature
+++ features/tutorial.feature
@@ -1,10 +1,13 @@
 Feature: showing off behave
 
+  @tutorial_test
+  @tutorial
   Scenario: run a simple test
      Given we have behave installed
       When we implement a test
       Then behave will test it for us!
 
+  @tutorial_test
   Scenario: explaining behave
      Given we put the system in a known state 
      # before the user (or external system) starts interacting with the system (in the When steps)
@@ -13,6 +16,7 @@ Feature: showing off behave
       # This is the interaction with your system which should (or perhaps should not) cause some state to change
       Then we observe outcomes
 
+  @outline_example
   Scenario Outline: Blenders
    Given I put <thing> in a blender
     when I switch the blender on
@@ -26,3 +30,34 @@ Feature: showing off behave
      | thing         | other thing |
      | iPhone        | toxic waste |
      | Galaxy Nexus  | toxic waste |
+
+  @step_data
+  Scenario: some scenario
+   Given a sample text loaded into the frobulator
+     """
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+     eiusmod tempor incididunt ut labore et dolore magna aliqua.
+     """
+    When we activate the frobulator
+    Then we will find it similar to English
+
+  @step_data
+  Scenario: another some scenario
+   Given a set of specific users
+      | name      | department  |
+      | Barry     | Beer Cans   |
+      | Pudey     | Silly Walks |
+      | Two-Lumps | Silly Walks |
+
+    When we count the number of people in each department
+    Then we will find two people in "Silly Walks"
+     But we will find one person in "Beer Cans"
+
+  @step_params
+  Scenario: look up a book
+    Given I search for a valid book
+     Then the result page will include "success"
+
+  Scenario: look up an invalid book
+    Given I search for an invalid book
+     Then the result page will include "failure"
