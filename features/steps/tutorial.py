130a2f986f5ed497a5891e1b3656692d5547ccfc
Jed Cainglet <jedcainglet@gmail.com>
2021-02-07 21:02:54 -0500

    Add initial Tutorial work

--- features/steps/tutorial.py
+++ features/steps/tutorial.py
@@ -11,3 +11,58 @@ def step_impl(context):
 @then('behave will test it for us!')
 def step_impl(context):
     assert context.failed is False
+
+
+@given('we put the system in a known state')
+def step_impl(context):
+    pass
+
+@when('we take key actions the user (or external system) performs')
+def step_impl(context):
+    assert True is not False
+
+@then('we observe outcomes')
+def step_impl(context):
+    assert context.failed is False
+
+
+@given('I put Red Tree Frog in a blender')
+def step_impl(context):
+    pass
+
+@when('I switch the blender on')
+def step_impl(context):
+    assert True is not False
+
+@then('it should transform into mush')
+def step_impl(context):
+    assert context.failed is False
+
+
+@given('I put iPhone in a blender')
+def step_impl(context):
+    pass
+
+# NOTE: already defined
+# @when('I switch the blender on')
+# def step_impl(context):
+#     assert True is not False
+
+@then('it should transform into toxic waste')
+def step_impl(context):
+    assert context.failed is False
+
+
+@given('I put Galaxy Nexus in a blender')
+def step_impl(context):
+    pass
+
+# NOTE: already defined
+# @when('I switch the blender on')
+# def step_impl(context):
+#     assert True is not False
+
+# NOTE: already defined
+# @then('it should transform into toxic waste')
+# def step_impl(context):
+#     assert context.failed is False
--- features/tutorial.feature
+++ features/tutorial.feature
@@ -4,3 +4,25 @@ Feature: showing off behave
      Given we have behave installed
       When we implement a test
       Then behave will test it for us!
+
+  Scenario: explaining behave
+     Given we put the system in a known state 
+     # before the user (or external system) starts interacting with the system (in the When steps)
+     # Avoid talking about user interaction in givens
+      When we take key actions the user (or external system) performs 
+      # This is the interaction with your system which should (or perhaps should not) cause some state to change
+      Then we observe outcomes
+
+  Scenario Outline: Blenders
+   Given I put <thing> in a blender
+    when I switch the blender on
+    then it should transform into <other thing>
+
+   Examples: Amphibians
+     | thing         | other thing |
+     | Red Tree Frog | mush        |
+
+   Examples: Consumer Electronics
+     | thing         | other thing |
+     | iPhone        | toxic waste |
+     | Galaxy Nexus  | toxic waste |
